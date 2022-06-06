6041b912a70c1be2e8e0b84c5d0bc33a718de914
Bhumikarai <81767312+Bhumikarai@users.noreply.github.com>
2022-06-06 17:05:06 +0545

    Food website

---
+++ Food_website/JS/Checkout.js
@@ -0,0 +1,4 @@
+let checkinbtn = document.querySelector(".checkinbtn");
+checkinbtn.addEventListener('click', ()=>{
+	alert('Thankyou for shopping with us!! Please visit again.');
+});
---
+++ Food_website/JS/Remove.js
@@ -0,0 +1,4 @@
+let removebtn = document.querySelector(".removebtn");
+removebtn.addEventListener('click', ()=>{
+	alert(' Your item has been successfully removed from the bag.Thankyou for shopping with us!! ');
+});
